f561b633b8ac3a869f93153883740c6326708feb
Arthur Allamand <a.allamand@gmail.com>
2017-07-31 15:14:58 +0200

    generating pages dynamically

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flat.each do |owner, flat|
+   proxy "/#{owner}.html", "/template.html", locals: { owner: owner }, :ignore => true
+end
